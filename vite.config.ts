1fe8f76118e836fdf8c6f44f445d4768ff256817
smileystudio <smileystudio.in@gmail.com>
2026-01-06 13:09:04 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@
   import path from 'path';
 
   export default defineConfig({
-    base: '/sukha-yatra/',
+    base: '/sukha-yatra-web/',
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
